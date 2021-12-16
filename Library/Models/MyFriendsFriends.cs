ca9269b400a61a6582d4dccd9372528797143b8b
Christopher B <72949595+ChristopherBerglund@users.noreply.github.com>
2021-12-16 14:09:32 +0100

    text file

---
+++ Library/Models/MyFriendsFriends.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class MyFriendsFriends
+    {
+        public int ID { get; set; }
+        public int myFriendID { get; set; }
+        public string userName { get; set; }
+        public string Email { get; set; }
+
+        public MyFriendsFriends()
+        {
+
+        }
+        public MyFriendsFriends(string[] bodymessage)
+        {
+            this.userName = bodymessage[0];
+            this.Email = bodymessage[1];
+        }
+    }
+}
