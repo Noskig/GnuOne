bc1abcd56a5abcd76e377c7560434505b7645e55
johannamaria91 <70133976+johannamaria91@users.noreply.github.com>
2022-01-03 17:10:03 +0100

    fetch and display list of friends

--- GnuOne/ClientApp/src/App.js
+++ GnuOne/ClientApp/src/App.js
@@ -8,17 +8,19 @@ import {
 } from "react-router-dom";
 import PortContext from './contexts/portContext'
 import MyProfile from './Pages/MyProfile';
+import Friends from './components/NewComponents/Friends';
 
 function App() {
     return (
         <PortContext.Provider value={7261}>
-        <Router>
-            <div className="App">
+            <Router>
+                <div className="App">
                     <Switch>
                         <Route exact path="/"> <MyProfile /></Route>
-                    <Route path="/discussions/:id"><Discussion /></Route>
-                </Switch>
-            </div>
+                        <Route path="/friends"><Friends /> </Route> 
+                        <Route path="/discussions/:id"><Discussion /></Route>
+                    </Switch>
+                </div>
             </Router>
         </PortContext.Provider>
     )
--- GnuOne/ClientApp/src/components/NewComponents/Friends.js
+++ GnuOne/ClientApp/src/components/NewComponents/Friends.js
@@ -1,1 +1,69 @@
 ﻿
+import { useState, useEffect, useContext } from 'react'
+import PortContext from '../../contexts/portContext';
+
+
+const Friends = () => {
+
+
+    const port = useContext(PortContext)
+    const url = `https://localhost:${port}/api/myfriends`
+    const [friendsList, setFriendsList] = useState([])
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    async function fetchData() {
+        const response = await fetch(url)
+        const friends = await response.json()
+        console.log(friends)
+        setFriendsList(friends)
+    }
+    //function handleClick(e) {
+    //    e.preventDefault()
+    //    let topicData = {
+    //        headline: headline,
+    //        discussiontext: description,
+    //        user: user,
+    //    }
+
+    //    if (headline === "" || description === "") {
+    //        alert("Fill the missing fields please")
+    //    }
+    //    else {
+    //        addNewPost(topicData)
+    //    }
+
+    //}
+
+    async function friendRequest(friendRequestEmail) {
+        await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(friendRequestEmail),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            }
+        })
+        fetchData();
+    }
+
+  
+
+
+    return (
+
+        <section className="friends-container">
+            <div className="addFriend"> </div>
+            <div className="friends-list">
+                <ul>
+                    {friendsList.map(friend => <li>{friend.Email}</li>)}
+                </ul>
+            </div>
+
+        </section>
+
+    )
+}
+
+export default Friends
