5a1e19165ad522d5e191899e6238d29e47a824f9
Daniel Khoshnejad <Danielkhosh9@gmail.com>
2022-01-10 15:58:36 +0100

    new grid and menu wheel

--- GnuOne/ClientApp/src/Pages/MyProfile.js
+++ GnuOne/ClientApp/src/Pages/MyProfile.js
@@ -1,7 +1,8 @@
 ﻿import React, { useState } from 'react';
 import { Link, Switch } from 'react-router-dom';
-import friends from '../icons/friends.svg'
+import friends from '../icons/friends.svg';
 import ProfileWheel from '../components/NewComponents/ProfileWheel';
+import "./MyProfile.min.css";
 
 
 //testar
