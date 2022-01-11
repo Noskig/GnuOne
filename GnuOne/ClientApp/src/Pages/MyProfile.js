7ef431edafd862384d75c13a75cbd7d5c57bfc38
Boris <70134092+reezla@users.noreply.github.com>
2022-01-11 09:22:23 +0100

    navbar start, icons

--- GnuOne/ClientApp/src/Pages/MyProfile.js
+++ GnuOne/ClientApp/src/Pages/MyProfile.js
@@ -2,6 +2,7 @@
 import { Link, Switch } from 'react-router-dom';
 import friends from '../icons/friends.svg'
 import ProfileWheel from '../components/NewComponents/ProfileWheel';
+import Navbar from '../components/NewComponents/NavBar';
 
 
 //testar
@@ -10,7 +11,7 @@ function MyProfile({ routes }) {
 
     return (
         <main className="main">
-
+            <Navbar />
             <ProfileWheel routes={routes}/>
             
         </main>
