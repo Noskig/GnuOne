900c76d0695e8ebd35cf560cc8cde9eebca3ed58
johannamaria91 <70133976+johannamaria91@users.noreply.github.com>
2022-01-11 08:59:24 +0100

    ???

--- GnuOne/ClientApp/src/Pages/FriendProfile.js
+++ GnuOne/ClientApp/src/Pages/FriendProfile.js
@@ -1,4 +1,37 @@
-﻿const FriendProfile = () => {
+﻿import { useParams } from "react-router-dom"
+import { useEffect, useState, useContext } from 'react'
+import PortContext from '../contexts/portContext';
+
+const FriendProfile = () => {
+    const port = useContext(PortContext)
+    const url = `https://localhost:${port}/api/myfriends/`
+    const { email } = useParams()
+    const [friends, setFriends] = useState()
+
+ 
+ 
+
+    //useEffect(() => {
+    //    fetchData()
+    //}, [email])
+
+    //async function fetchData() {
+    //    console.log('fetching')
+    //    const response = await fetch(url, {
+    //        method: 'GET',
+    //        body: JSON.stringify({ Email: email }),
+    //        headers: {
+    //            "Content-type": "application/json; charset=UTF-8",
+    //        }
+    //    })
+    //    const friends = await response.json()
+    //    console.log(friends)
+    //    setFriends(friends);
+       
+    //}
+
+ 
+
 
 
 
@@ -7,6 +40,7 @@
         <section className="">
 
             <h3>Friend's profile</h3>
+        
 
         </section>
 
