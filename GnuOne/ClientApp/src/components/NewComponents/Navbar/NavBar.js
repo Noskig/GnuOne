3309f9738f736b492e0f3b559fc573a6e28c822d
Boris <70134092+reezla@users.noreply.github.com>
2022-01-12 08:59:55 +0100

    navbar

--- GnuOne/ClientApp/src/components/NewComponents/Navbar/NavBar.js
+++ GnuOne/ClientApp/src/components/NewComponents/Navbar/NavBar.js
@@ -1,15 +1,40 @@
 ﻿import React from 'react';
 import logo from '../../../icons/Logo.svg'
 import avatar from '../../../icons/useravatar.svg'
+import settings from '../../../icons/settings.svg'
+import messages from '../../../icons/messagesNavbar.svg'
+
 import "./navbar.css"
 
 const NavBar = () => {
 
     return (
             <section>
-                <div className="navbar">
-                    <img className="logo" src={logo} alt="logo"/>
-                    <img className="avatar" src={avatar} alt="avatar" />
+            <div className="navbar">
+                <ul id="menu">
+                    <a class="menu-button icon-plus" href="#menu" title="Show navigation"></a>
+                    <a class="menu-button icon-minus" href="#0" title="Hide navigation"></a>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span class="fa fa-github"></span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span class="fa fa-linkedin"></span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span class="fa fa-instagram"></span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span class="fa fa-twitter"></span>
+                        </a>
+                    </li>
+                </ul>
                 </div>
             </section>
         )
