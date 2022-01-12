2637f8fc65ad9ef7f3b2e22c90570de759a97ff7
Daniel Khoshnejad <Danielkhosh9@gmail.com>
2022-01-12 16:51:07 +0100

    overlay is fixed

--- GnuOne/ClientApp/src/components/NewComponents/DeleteDiscussionOverlay.js
+++ GnuOne/ClientApp/src/components/NewComponents/DeleteDiscussionOverlay.js
@@ -36,14 +36,14 @@ const DeleteDiscussionOverlay = (props) => {
     console.log(props)
 
     return (
-        <div className="" >
-            <section className="">
+        <div className="delete-overlay-background" >
+            <section className="delete-overlay-section">
 
-                <button className="close" onClick={close}>✖️</button>
-                <h4>Are you sure you want to delete this topic with all its content?</h4>
-                <div>
-                    <button className="button-close" onClick={close}> Cancel</button>
-                    <button className="button-delete" onClick={(e) => deleteDiscussion(e, props.discussionId)}>Confirm</button>
+                <button className="close button" onClick={close}>✖️</button>
+                <p className="conferm-text">Are you sure you want to delete this topic with all its content?</p>
+                <div className="button-container">
+                    <button className="button-close button" onClick={close}> Cancel</button>
+                    <button className="button-delete button" onClick={(e) => deleteDiscussion(e, props.discussionId)}>Confirm</button>
                 </div>
             </section>
         </div>
