0cc9b8f167edce1ff36c5303792ccccc233aee70
Boris <70134092+reezla@users.noreply.github.com>
2022-01-21 11:25:18 +0100

    ClientChat / ServerChat mapp

---
+++ GnuOne/ClientApp/ServerChat/index.js
@@ -0,0 +1,12 @@
+﻿const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+
+app.use(cors());
+
+const server = http.createServer(app)
+
+server.listen(7261, () => {
+    console.log("server is running")
+});
