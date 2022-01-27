f64a504c3e3657416eec97051c41f79b88c98537
johannamaria91 <70133976+johannamaria91@users.noreply.github.com>
2022-01-27 12:03:08 +0100

    i tried

--- GnuOne/ClientApp/src/components/Notifications/Notification.js
+++ GnuOne/ClientApp/src/components/Notifications/Notification.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useContext,useEffect } from 'react';
+﻿import React, { useState, useContext, useEffect, useCallback } from 'react';
 import { Link, Switch, useRouteMatch } from 'react-router-dom';
 import PortContext from '../../contexts/portContext';
 
@@ -9,37 +9,69 @@ function Notifications() {
 
     const port = useContext(PortContext);
     const url = `https://localhost:${port}/api/notifications`;
-    const [allNotifications, setAllNotifications] = useState([]);
+    const [allNotifications, setAllNotifications] = useState();
+    const [notifications, setNotifications] = useState()
+
+    const blabla = useCallback(async () => {
+        console.log(allNotifications)
+        let posts = allNotifications?.filter(notification => notification.Type === "post")
+        let discussions = allNotifications?.filter(notification => notification.Type === "discussion")
+        let theRest = allNotifications?.filter(notification => notification.Type !== "discussion" && notification.Type !== "post")
+        let newList = [];
+         posts.forEach(async (post) => {
+            const responseOne = await fetch(`https://localhost:${port}/api/posts/${post.infoID}`);
+            const actualPost = await responseOne.json();
+            post.discussionEmail = actualPost.discussionEmail.substring(0, actualPost.discussionEmail.lastIndexOf("@"));
+
+        })
+         discussions.forEach(async (discussion) => {
+            const responseTwo = await fetch(`https://localhost:${port}/api/discussions/${discussion.infoID}`);
+            const actualDiscussion = await responseTwo.json();
+            discussion.discussionEmail = actualDiscussion.email.substring(0, actualDiscussion.email.lastIndexOf("@"));
+
+        })
+
+        await newList.push(...posts, ...discussions, ...theRest)
+        console.log(newList)
+        setNotifications(newList)
+    }, [allNotifications, port])
 
     useEffect(() => {
         fetchData();
+        blabla();
     }, [])
 
     async function fetchData() {
         const response = await fetch(url);
         const notifications = await response.json();
         console.log(notifications)
-        //Skapar en ny lista "posts" genom att filtrera fram alla notifications av type "post"
-        let posts = notifications.filter(notification => notification.Type ===  "post")
-        console.log(posts)
 
-        let discussions = notifications.filter(notification => notification.Type === "discussion")
+     
+       // //Skapar en ny lista "posts" genom att filtrera fram alla notifications av type "post"
+       // let posts = notifications.filter(notification => notification.Type ===  "post")
+       // console.log(posts)
 
-        //Lägger på "discussionEmail" för varje post i listan "posts"
-        posts.forEach(async (post) => {
-            const responseOne = await fetch(`https://localhost:${port}/api/posts/${post.infoID}`);
-            const actualPost = await responseOne.json();
-            post.discussionEmail = actualPost.discussionEmail.substring(0, actualPost.discussionEmail.lastIndexOf("@"));
-        })
+       // let discussions = notifications.filter(notification => notification.Type === "discussion")
 
-        discussions.forEach(async (discussion) => {
-            const responseTwo = await fetch(`https://localhost:${port}/api/discussions/${discussion.infoID}`);
-            const actualDiscussion = await responseTwo.json();
-            discussion.discussionEmail = actualDiscussion.email.substring(0, actualDiscussion.email.lastIndexOf("@"));
-        })
+       // //Lägger på "discussionEmail" för varje post i listan "posts"
+       //await posts.forEach(async (post) => {
+       //     const responseOne = await fetch(`https://localhost:${port}/api/posts/${post.infoID}`);
+       //     const actualPost = await responseOne.json();
+       //     post.discussionEmail = actualPost.discussionEmail.substring(0, actualPost.discussionEmail.lastIndexOf("@"));
+       // })
+
+        //await discussions.forEach(async (discussion) => {
+        //    const responseTwo = await fetch(`https://localhost:${port}/api/discussions/${discussion.infoID}`);
+        //    const actualDiscussion = await responseTwo.json();
+        //    discussion.discussionEmail = actualDiscussion.email.substring(0, actualDiscussion.email.lastIndexOf("@"));
+        //})
+  
         setAllNotifications(notifications);
         console.log(notifications)
     }
+
+    
+
 
     function setId(newEvents) {
         deleteNotification(newEvents)
@@ -56,7 +88,7 @@ function Notifications() {
 
     return (
         <section className="notification-section">
-            {allNotifications.map(newEvents => 
+            {notifications?.map(newEvents => 
                 newEvents.Type === "FriendRequestRecieved" ?
                     <div key={newEvents.ID}>
                         <p>you have a new friend request from {newEvents.userName}</p>
@@ -80,6 +112,7 @@ function Notifications() {
                             newEvents.Type === "post" ?
                                 <div key={newEvents.ID}>
                                     <Link>
+                                        <p></p>
                                         <p>you have {newEvents.Counter} new comments on your post "{newEvents.Headline}"</p>
                                     </Link>
                                         <button onClick={() => setId(newEvents.ID)}>Delete</button>
