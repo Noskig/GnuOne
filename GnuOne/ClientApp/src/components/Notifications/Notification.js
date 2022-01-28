16c480567d55bf20038ad8856505af419c56a9dc
johannamaria91 <70133976+johannamaria91@users.noreply.github.com>
2022-01-28 16:57:24 +0100

    notifications fixed

--- GnuOne/ClientApp/src/components/Notifications/Notification.js
+++ GnuOne/ClientApp/src/components/Notifications/Notification.js
@@ -9,7 +9,8 @@ function Notifications() {
     const myEmail = useContext(MeContext);
     const port = useContext(PortContext);
     const url = `https://localhost:${port}/api/notifications`;
-    const [allNotifications, setAllNotifications] = useState();
+    const [readNotifications, setReadNotifications] = useState();
+    const [unreadNotifications, setUnreadNotifications] = useState();
 
     useEffect(() => {
         fetchData();
@@ -19,11 +20,13 @@ function Notifications() {
         const response = await fetch(url);
         const notifications = await response.json();
         console.log(notifications)
-  
-        setAllNotifications(notifications);
+        let read = notifications.filter(notification => notification.hasBeenRead === true)
+        let unread = notifications.filter(notification => notification.hasBeenRead === false)
+        setReadNotifications(read)
+        setUnreadNotifications(unread);
     }
 
-    
+
 
 
     function setId(newEvents) {
@@ -31,12 +34,25 @@ function Notifications() {
     }
 
     async function deleteNotification(notificationId) {
-        await fetch(`https://localhost:${port}/api/notifications/${notificationId}`, {
-            method: 'DELETE',
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            }
-        })
+        if (typeof(notificationId) === "object") {
+            notificationId.forEach(async (id) => {
+                await fetch(`https://localhost:${port}/api/notifications/${id.ID}`, {
+                    method: 'DELETE',
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8",
+                    }
+                })
+            })
+        } else {
+            await fetch(`https://localhost:${port}/api/notifications/${notificationId}`, {
+                method: 'DELETE',
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                }
+            })
+        }
+
+
     }
 
     async function markAsRead(notificationId) {
@@ -46,38 +62,81 @@ function Notifications() {
                 "Content-type": "application/json; charset=UTF-8",
             }
         })
-      
+
     }
 
     return (
         <section className="notification-section">
-            {allNotifications?.map(newEvents =>
+            <h3>Unread notifications</h3>
+            {unreadNotifications?.map(newEvents =>
                 newEvents.Type === "FriendRequestRecieved" ?
-                    <div key={newEvents.ID} className={newEvents.hasBeenRead ? "read" : "unread"}>
-                        <p>you have a new friend request from {newEvents.userName}</p>
-                        <button onClick={() => setId(newEvents.ID)}>Delete</button>
+                    <div key={newEvents.ID} className="unread notification">
+                        <Link onClick={() => markAsRead(newEvents.ID)} to={'profile/friends'}>
+                            <p>You have a new friend request from {newEvents.userName}</p>
+                        </Link>
+                        <button className="delete-notification" onClick={() => setId(newEvents.ID)}>Delete notification</button>
                     </div>
                     :
                     newEvents.Type === "FriendRequestAccepted" ?
-                        <div key={newEvents.ID}>
-                            <p>{newEvents.userName} has accepted your friend request</p>
-                            <button onClick={() => setId(newEvents.ID)}>Delete</button>
+                        <div key={newEvents.ID} className="unread notification">
+                            <Link onClick={() => markAsRead(newEvents.ID)} to={'profile/friends'}>
+                                <p>{newEvents.userName} has accepted your friend request</p>
+                            </Link>
+                            <button className="delete-notification" onClick={() => setId(newEvents.ID)}>Delete notification</button>
                         </div>
                         :
                         newEvents.Type === "discussion" ?
-                            <div key={newEvents.ID} className={newEvents.hasBeenRead ? "read" : "unread"} >
-                                <Link to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.infoID}`}>
-                                    <p>you have {newEvents.Counter} new post on "{newEvents.Headline}"</p>
+                            <div key={newEvents.ID} className="unread notification" >
+                                <Link onClick={() => markAsRead(newEvents.ID)} to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.infoID}`}>
+                                    <p>You have {newEvents.Counter} new post(s) on "{newEvents.Headline}"</p>
                                 </Link>
-                                    <button onClick={() => setId(newEvents.ID)}>Delete</button>
+                                <button className="delete-notification" onClick={() => setId(newEvents.ID)}>Delete notification</button>
                             </div>
                             :
                             newEvents.Type === "post" ?
-                                <div key={newEvents.ID} className={newEvents.hasBeenRead ? "read" : "unread"}>
-                                    <Link onClick={()=>markAsRead(newEvents.ID)}to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}`}>
-                                        <p>you have {newEvents.Counter} new comments on your post "{newEvents.Headline}"</p>
+                                <div key={newEvents.ID} className="unread notification">
+                                    <Link onClick={() => markAsRead(newEvents.ID)} to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}`}>
+                                        <p>You have {newEvents.Counter} new comment(s) on your post "{newEvents.Headline}"</p>
                                     </Link>
-                                        <button onClick={() => setId(newEvents.ID)}>Delete</button>
+                                    <button className="delete-notification" onClick={() => setId(newEvents.ID)}>Delete notification</button>
+                                </div>
+                                : <h1>no new notifications</h1>
+            )}
+            <div className="notifications-header">
+                <h3>Read notifications</h3>
+                <h4 onClick={() => deleteNotification(readNotifications)}>Delete all read notifications</h4>
+            </div>
+            {readNotifications?.map(newEvents =>
+                newEvents.Type === "FriendRequestRecieved" ?
+                    <div key={newEvents.ID} className="read notification">
+                        <Link to={'profile/friends'}>
+                            <p>you have a new friend request from {newEvents.userName}</p>
+                        </Link>
+                        <button className="delete-notification" onClick={() => setId(newEvents.ID)}>Delete notification</button>
+                    </div>
+                    :
+                    newEvents.Type === "FriendRequestAccepted" ?
+                        <div key={newEvents.ID} className="read notification">
+                            <Link to={'profile/friends'}>
+                                <p>{newEvents.userName} has accepted your friend request</p>
+                            </Link>
+                            <button className="delete-notification" onClick={() => setId(newEvents.ID)}>Delete notification</button>
+                        </div>
+                        :
+                        newEvents.Type === "discussion" ?
+                            <div key={newEvents.ID} className="read notification" >
+                                <Link to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.infoID}`}>
+                                    <p>You have {newEvents.Counter} new posts on "{newEvents.Headline}"</p>
+                                </Link>
+                                <button className="delete-notification" onClick={() => setId(newEvents.ID)}>Delete notification</button>
+                            </div>
+                            :
+                            newEvents.Type === "post" ?
+                                <div key={newEvents.ID} className="read notification">
+                                    <Link onClick={() => markAsRead(newEvents.ID)} to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}`}>
+                                        <p>You have {newEvents.Counter} new comments on your post "{newEvents.Headline}"</p>
+                                    </Link>
+                                    <button onClick={() => setId(newEvents.ID)}>Delete</button>
                                 </div>
                                 : <h1>no new notifications</h1>
             )}
