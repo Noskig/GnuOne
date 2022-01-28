01c399844728f0e5d72f7c94bd39288590e052f1
johannamaria91 <70133976+johannamaria91@users.noreply.github.com>
2022-01-28 13:17:03 +0100

    trying to implement has been read

--- GnuOne/ClientApp/src/components/Notifications/Notification.js
+++ GnuOne/ClientApp/src/components/Notifications/Notification.js
@@ -2,41 +2,14 @@
 import { Link, Switch, useRouteMatch } from 'react-router-dom';
 import PortContext from '../../contexts/portContext';
 import MeContext from '../../contexts/meContext'
-
+import './notifications.css'
 
 //testar
 function Notifications() {
-
     const myEmail = useContext(MeContext);
     const port = useContext(PortContext);
     const url = `https://localhost:${port}/api/notifications`;
     const [allNotifications, setAllNotifications] = useState();
-
-    /*const [notifications, setNotifications] = useState()*/
-
-    //const blabla = useCallback(async () => {
-    //    console.log(allNotifications)
-    //    let posts = allNotifications?.filter(notification => notification.Type === "post")
-    //    let discussions = allNotifications?.filter(notification => notification.Type === "discussion")
-    //    let theRest = allNotifications?.filter(notification => notification.Type !== "discussion" && notification.Type !== "post")
-    //    let newList = [];
-    //     posts.forEach(async (post) => {
-    //        const responseOne = await fetch(`https://localhost:${port}/api/posts/${post.infoID}`);
-    //        const actualPost = await responseOne.json();
-    //        post.discussionEmail = actualPost.discussionEmail.substring(0, actualPost.discussionEmail.lastIndexOf("@"));
-
-    //    })
-    //     discussions.forEach(async (discussion) => {
-    //        const responseTwo = await fetch(`https://localhost:${port}/api/discussions/${discussion.infoID}`);
-    //        const actualDiscussion = await responseTwo.json();
-    //        discussion.discussionEmail = actualDiscussion.email.substring(0, actualDiscussion.email.lastIndexOf("@"));
-
-    //    })
-
-    //    await newList.push(...posts, ...discussions, ...theRest)
-    //    console.log(newList)
-    //    setNotifications(newList)
-    //}, [allNotifications, port])
 
     useEffect(() => {
         fetchData();
@@ -46,29 +19,8 @@ function Notifications() {
         const response = await fetch(url);
         const notifications = await response.json();
         console.log(notifications)
-
-     
-       // //Skapar en ny lista "posts" genom att filtrera fram alla notifications av type "post"
-       // let posts = notifications.filter(notification => notification.Type ===  "post")
-       // console.log(posts)
-
-       // let discussions = notifications.filter(notification => notification.Type === "discussion")
-
-       // //Lägger på "discussionEmail" för varje post i listan "posts"
-       //await posts.forEach(async (post) => {
-       //     const responseOne = await fetch(`https://localhost:${port}/api/posts/${post.infoID}`);
-       //     const actualPost = await responseOne.json();
-       //     post.discussionEmail = actualPost.discussionEmail.substring(0, actualPost.discussionEmail.lastIndexOf("@"));
-       // })
-
-        //await discussions.forEach(async (discussion) => {
-        //    const responseTwo = await fetch(`https://localhost:${port}/api/discussions/${discussion.infoID}`);
-        //    const actualDiscussion = await responseTwo.json();
-        //    discussion.discussionEmail = actualDiscussion.email.substring(0, actualDiscussion.email.lastIndexOf("@"));
-        //})
   
         setAllNotifications(notifications);
-        console.log(notifications)
     }
 
     
@@ -87,11 +39,21 @@ function Notifications() {
         })
     }
 
+    async function markAsRead(notificationId) {
+        await fetch(`https://localhost:${port}/api/notifications/${notificationId}`, {
+            method: 'PUT',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            }
+        })
+      
+    }
+
     return (
         <section className="notification-section">
             {allNotifications?.map(newEvents =>
                 newEvents.Type === "FriendRequestRecieved" ?
-                    <div key={newEvents.ID}>
+                    <div key={newEvents.ID} className={newEvents.hasBeenRead ? "read" : "unread"}>
                         <p>you have a new friend request from {newEvents.userName}</p>
                         <button onClick={() => setId(newEvents.ID)}>Delete</button>
                     </div>
@@ -103,7 +65,7 @@ function Notifications() {
                         </div>
                         :
                         newEvents.Type === "discussion" ?
-                            <div key={newEvents.ID}>
+                            <div key={newEvents.ID} className={newEvents.hasBeenRead ? "read" : "unread"} >
                                 <Link to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.infoID}`}>
                                     <p>you have {newEvents.Counter} new post on "{newEvents.Headline}"</p>
                                 </Link>
@@ -111,8 +73,8 @@ function Notifications() {
                             </div>
                             :
                             newEvents.Type === "post" ?
-                                <div key={newEvents.ID}>
-                                    <Link to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}`}>
+                                <div key={newEvents.ID} className={newEvents.hasBeenRead ? "read" : "unread"}>
+                                    <Link onClick={()=>markAsRead(newEvents.ID)}to={myEmail === newEvents.Email ? `profile/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}` : `friendprofile/${newEvents.Email.substring(0, newEvents.Email.lastIndexOf("@"))}/discussions/${newEvents.DiscussionID}/post/${newEvents.infoID}`}>
                                         <p>you have {newEvents.Counter} new comments on your post "{newEvents.Headline}"</p>
                                     </Link>
                                         <button onClick={() => setId(newEvents.ID)}>Delete</button>
